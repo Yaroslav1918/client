a7bd072da99d32168c76fe3bb3c33db1b893f0d4
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2025-02-02 13:10:19 +0200

    add client folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = "client";
+  http = inject(HttpClient);
+  users: any;
+  
+  ngOnInit(): void {
+    
+  }
 }
